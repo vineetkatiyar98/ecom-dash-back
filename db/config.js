730a65f074824620ff7b53945a079db6b6bc3be2
vineetkatiyar98 <104678201+vineetkatiyar98@users.noreply.github.com>
2023-09-16 03:07:56 +0530

    change commit

--- db/config.js
+++ db/config.js
@@ -1,11 +1,13 @@
-const mongoose = require('mongoose');
-require ("dotenv").config()
-mongoose.connect(process.env.MONGO_URL,{
+dotenv.config();
+
+mongoose.connect(
+  process.env.MONGO_URL,
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-}).then(()=>{
-    console.log(`connected to ${process.env.DB_NAME}`);
-}).catch((error)=>{
-    console.log(`${error} connecting to ${process.env.DB_NAME}`)
-})
+  },
+  () => {
+    console.log("mongdb is connected");
+  }
+);
