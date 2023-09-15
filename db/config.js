a1165ac8bf5443db9b824ce7647630941918781c
vineetkatiyar98 <104678201+vineetkatiyar98@users.noreply.github.com>
2023-09-16 03:44:41 +0530

    change commit

--- db/config.js
+++ db/config.js
@@ -3,7 +3,8 @@ const dotenv = require("dotenv");
 require('dotenv').config()
 
 mongoose.connect(process.env.MONGO_URL,{
-    useNewUrlParser: true, useUnifiedTopology: true 
+    useNewUrlParser: true, useUnifiedTopology: true ,
+    serverSelectionTimeoutMS: 30000
 }).then(()=>{
     console.log(`connected to ${process.env.DB_NAME}`);
 }).catch((error)=>{
