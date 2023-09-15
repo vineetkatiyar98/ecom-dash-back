935acbd00e571f98d55d24d9b88eb1ce6049778d
vineetkatiyar98 <104678201+vineetkatiyar98@users.noreply.github.com>
2023-09-16 02:36:04 +0530

    change commit

--- db/User.js
+++ db/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     name: String,
     phone : Number,
     email: String,
-    password: String
+    password: String    
 });
 
 module.exports = mongoose.model("users", userSchema);
--- index.js
+++ index.js
@@ -9,24 +9,22 @@ const app = express();
 const port = process.env.PORT || 8000;
 app.use(express.json());
 app.use(cors());
-// const Jwt = require('jsonwebtoken');
-// const jwtKey = 'e-com';
+
 
 app.post("/register", async (req, resp) => {
-  let user = new User(req.body);
-  let result = await user.save();
-  result = result.toObject();
-  delete result.password;
-  resp.send(result);
-  //   Jwt.sign({ result }, jwtKey, { expiresIn: "2h" }, (err, token) => {
-  //     if (err) {
-  //       resp.send("Something went wrong");
-  //     }
-  //     resp.send({ result, auth: token });
-  //   });
+  try{
+    let user = new User(req.body);
+    let result = await user.save();
+    result = result.toObject();
+    delete result.password;
+    resp.send(result);
+  }catch(e){
+    resp.status(409).send(e.message);
+  }
 });
 
 app.post("/login", async (req, resp) => {
+try{
   if (req.body.password && req.body.email) {
     let user = await User.findOne(req.body).select("-password");
     if (user) {
@@ -39,70 +37,107 @@ app.post("/login", async (req, resp) => {
   } else {
     resp.send({ result: "No User found" });
   }
+}catch(e){
+  resp.status(409).send(e.message);
+}
 });
 
 app.post("/add-product", async (req, resp) => {
-  let product = new Product(req.body);
-  let result = await product.save();
-  resp.send(result);
+  try{
+    let product = new Product(req.body);
+    let result = await product.save();
+    resp.send(result);
+  }
+  catch(e){
+  resp.status(409).send(e.message);
+
+  }
 });
 
 app.get("/products", async (req, resp) => {
+try{
   const products = await Product.find();
   if (products.length > 0) {
       resp.send(products)
   } else {
       resp.send({ result: "No Product found" })
   }
+}  catch(e){
+  resp.status(409).send(e.message);
+
+  }
 });;
 
 app.delete("/product/:id", async (req, resp) => {
-  let result = await Product.deleteOne({ _id: req.params.id });
-  resp.send(result)
+  try{
+    let result = await Product.deleteOne({ _id: req.params.id });
+    resp.send(result)
+  } catch(e){
+    resp.status(409).send(e.message);
+  
+    }
 }),
 
 
 app.get("/product/:id", async (req, resp) => {
+try{
   let result = await Product.findOne({ _id: req.params.id })
   if (result) {
       resp.send(result)
   } else {
       resp.send({ "result": "No Record Found." })
   }
+}
+catch(e){
+  resp.status(409).send(e.message);
+  }
 })
 
 
 app.put("/product/:id", async (req, resp) => {
-    let result = await Product.updateOne(
-        { _id: req.params.id },
-        { $set: req.body }
-    )
-    resp.send(result)
+try{
+  let result = await Product.updateOne(
+    { _id: req.params.id },
+    { $set: req.body }
+)
+resp.send(result)
+}catch(e){
+  resp.status(409).send(e.message);
+  }
 });
 
 app.put("/product/:id", async (req, resp) => {
+  try{
     let result = await Product.updateOne(
-        { _id: req.params.id },
-        { $set: req.body }
-    )
-    resp.send(result)
+      { _id: req.params.id },
+      { $set: req.body }
+  )
+  resp.send(result)
+  }catch(e){
+  resp.status(409).send(e.message);
+  }
 });
 
 app.get("/search/:key", async (req, resp) => {
-    let result = await Product.find({
-        "$or": [
-            {
-                name: { $regex: req.params.key }
-            },
-            {
-                company: { $regex: req.params.key }
-            },
-            {
-                category: { $regex: req.params.key }
-            }
-        ]
-    });
-    resp.send(result);
+try{
+  let result = await Product.find({
+    "$or": [
+        {
+            name: { $regex: req.params.key }
+        },
+        {
+            company: { $regex: req.params.key }
+        },
+        {
+            category: { $regex: req.params.key }
+        }
+    ]
+});
+resp.send(result);
+}
+catch (e) {
+  resp.status(409).send(e.message);
+}
 })
 
 app.listen(port, () => {
